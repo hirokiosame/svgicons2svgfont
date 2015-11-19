2c10fab468b6923230946c439a01ab7bdf30c3e5
Hiroki Osame <hiroki@weebly.com>
2015-11-18 17:52:09 -0800

    Reverted spacing

--- src/metadata.js
+++ src/metadata.js
@@ -6,13 +6,6 @@ var fs = require('fs');
 require('string.fromcodepoint');
 require('string.prototype.codepointat');
 
-function hex2a(hexx) {
-    var hex = hexx.toString();//force conversion
-    var str = '';
-    for (var i = 0; i < hex.length; i += 2)
-        str += String.fromCharCode(parseInt(hex.substr(i, 2), 16));
-    return str;
-}
 
 function getMetadataService(options) {
   var usedUnicodes = {};
